f00c79452cf42326917c69ea23e3310551463933
Stefan Cvetic <imbactbulletz@Stefans-MacBook-Pro.local>
2022-08-09 16:12:48 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/auth_wall/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.auth_wall
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AuthWallApp());
+}
+
+class AuthWallApp extends StatelessWidget {
+
+  const AuthWallApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO
+  }
+}
