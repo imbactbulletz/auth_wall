c359ed64ac2db08709b2db369cfaffe1f916e66a
imbactbulletz <cveticstefancpl@gmail.com>
2022-08-10 02:26:37 +0200

    Implement profile page functionality and navigation

--- lib/profile/ProfilePage.dart
+++ lib/profile/ProfilePage.dart
@@ -1,13 +1,56 @@
+import 'package:auth_wall/navigation/Navigation.gr.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import '../util/widgets/MaxSizedBox.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => const MaxSizedBox(
-      child: Center(
-        child: Text("Profile Page"),
+  Widget build(BuildContext context) => MaxSizedBox(
+          child: Center(
+        child: Column(
+          children: [
+            PlatformAppBar(
+              title: const Text("My Profile"),
+            ),
+            const Spacer(),
+            IntrinsicWidth(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  PlatformElevatedButton(
+                    child: const Text("Log in"),
+                    onPressed: () => AutoRouter.of(context).push(const LoginRoute()),
+                  ),
+                  PlatformElevatedButton(
+                    child: const Text("Register"),
+                    onPressed: () => showFeatureNotImplementedDialog(context),
+                  )
+                ],
+              ),
+            ),
+            const Spacer(),
+          ],
+        ),
       ));
 
+  void showFeatureNotImplementedDialog(BuildContext context) =>
+    showPlatformDialog(
+        context: context,
+        builder: (_) =>
+            PlatformAlertDialog(
+              title: const Text("Not implemented"),
+              content: const Text("This feature is not implemented yet"),
+              actions: [
+                PlatformDialogAction(
+                  child: const Text("Alright"),
+                  onPressed: () => {
+                    AutoRouter.of(context).pop()
+                  },
+                )
+              ],
+            )
+    );
 }
